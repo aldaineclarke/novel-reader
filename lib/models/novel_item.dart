c9d18bd7cb7e161a1832ad069789e3c80922a2b6
Aldaine-Legion <vainedev@gmail.com>
2024-06-09 12:53:46 -0500

    Added (Novel Item): Added model novel item for the landing page content

---
+++ lib/models/novel_item.dart
@@ -0,0 +1,38 @@
+class NovelItem {
+  NovelItem({
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.genres,
+    required this.image,
+    required this.lastChapter,
+  });
+
+  factory NovelItem.fromJson(Map<String, dynamic> json) {
+    return NovelItem(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      url: json['url'] as String,
+      genres: List<String>.from(json['genres'] as Iterable<dynamic>),
+      image: json['image'] as String,
+      lastChapter: json['lastChapter'] as String,
+    );
+  }
+  final String id;
+  final String title;
+  final String url;
+  final List<String> genres;
+  final String image;
+  final String lastChapter;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'url': url,
+      'genres': genres,
+      'image': image,
+      'lastChapter': lastChapter,
+    };
+  }
+}
