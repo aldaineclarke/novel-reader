5eb5797c131031ad9237045940e7fb2f439c995d
Aldaine-Legion <vainedev@gmail.com>
2024-07-20 17:10:42 -0500

    Updated (Shelf): Updated the Shelf to allow user to remove novel from the list

--- lib/pages/shelf.dart
+++ lib/pages/shelf.dart
@@ -82,66 +82,144 @@ class _ShelfScreenState extends ConsumerState<ShelfScreen> {
                         const SizedBox(width: 10),
                         Expanded(
                           child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(
-                                novelItem.novelTitle,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium
-                                    ?.copyWith(fontWeight: FontWeight.w600),
-                                softWrap: false,
-                                overflow: TextOverflow.fade,
-                                textAlign: TextAlign.start,
-                              ),
-                              Row(
+                              Expanded(
+                                  child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  const Icon(
-                                    Icons.timer_outlined,
-                                    color: Colors.lime,
-                                    size: 18,
+                                  Text(
+                                    novelItem.novelTitle,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyMedium
+                                        ?.copyWith(fontWeight: FontWeight.w600),
+                                    softWrap: false,
+                                    overflow: TextOverflow.fade,
+                                    textAlign: TextAlign.start,
                                   ),
-                                  const SizedBox(
-                                    width: 5,
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.timer_outlined,
+                                        color: Colors.lime,
+                                        size: 18,
+                                      ),
+                                      const SizedBox(
+                                        width: 5,
+                                      ),
+                                      Expanded(
+                                        child: Text(
+                                          '${novelItem.chapterCount}',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodySmall,
+                                          softWrap: false,
+                                          overflow: TextOverflow.fade,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  Expanded(
-                                    child: Text(
-                                      '${novelItem.chapterCount}',
-                                      style:
-                                          Theme.of(context).textTheme.bodySmall,
-                                      softWrap: false,
-                                      overflow: TextOverflow.fade,
+                                  const SizedBox(height: 10),
+                                  SizedBox(
+                                    child: Wrap(
+                                      runSpacing: 2,
+                                      spacing: 2,
+                                      children: novelItem.genres.map((e) {
+                                        return Container(
+                                          decoration: const BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.horizontal(
+                                              left: Radius.circular(10),
+                                              right: Radius.circular(10),
+                                            ),
+                                            color: Color.fromARGB(
+                                                255, 249, 255, 255),
+                                          ),
+                                          padding: const EdgeInsets.all(5),
+                                          child: Text(
+                                            e,
+                                            style: const TextStyle(
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.w500,
+                                              color: Color.fromARGB(
+                                                  255, 12, 58, 95),
+                                            ),
+                                          ),
+                                        );
+                                      }).toList(),
                                     ),
                                   ),
                                 ],
-                              ),
-                              const SizedBox(height: 10),
-                              SizedBox(
-                                child: Wrap(
-                                  runSpacing: 2,
-                                  spacing: 2,
-                                  children: novelItem.genres.map((e) {
-                                    return Container(
-                                      decoration: const BoxDecoration(
-                                        borderRadius: BorderRadius.horizontal(
-                                          left: Radius.circular(10),
-                                          right: Radius.circular(10),
+                              )),
+                              TextButton(
+                                onPressed: () {
+                                  showDialog<void>(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: const Text('Remove Novel'),
+                                        content: RichText(
+                                          text: TextSpan(
+                                            style: DefaultTextStyle.of(context)
+                                                .style, // Inherit default text style
+                                            children: <TextSpan>[
+                                              const TextSpan(
+                                                text:
+                                                    'Are you sure you want to remove the novel from your shelf? \n\n',
+                                                style: TextStyle(
+                                                    color: Colors
+                                                        .black), // Customize the style
+                                              ),
+                                              TextSpan(
+                                                text: '${novelItem.novelTitle}',
+                                                style: const TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize:
+                                                        18), // Customize the style
+                                              ),
+                                            ],
+                                          ),
                                         ),
-                                        color:
-                                            Color.fromARGB(255, 249, 255, 255),
-                                      ),
-                                      padding: const EdgeInsets.all(5),
-                                      child: Text(
-                                        e,
-                                        style: const TextStyle(
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.w500,
-                                          color:
-                                              Color.fromARGB(255, 12, 58, 95),
-                                        ),
-                                      ),
-                                    );
-                                  }).toList(),
+                                        actionsAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text(
+                                              'Cancel',
+                                              style: TextStyle(
+                                                  color: Colors.black26),
+                                            ),
+                                          ),
+                                          const Expanded(child: SizedBox()),
+                                          TextButton(
+                                            onPressed: () {
+                                              final novelIndex =
+                                                  shelf.indexOf(novelItem);
+                                              ref
+                                                  .read(shelfProvider.notifier)
+                                                  .removeFromShelf(novelIndex);
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text(
+                                              'Remove Novel',
+                                              style:
+                                                  TextStyle(color: Colors.red),
+                                            ),
+                                          )
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                                child: const Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text('Remove Novel'),
+                                    // Icon(Icons.delete_rounded),
+                                  ],
                                 ),
                               )
                             ],
