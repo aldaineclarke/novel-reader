1d7536d388250acc15209bb41f0e6851d8999fbc
Aldaine-Legion <vainedev@gmail.com>
2024-10-08 07:29:18 -0500

    Updated (Bottom Nav): Updated the bottom nav to use the keyboard visibility provider to determine if it should hide or show.

--- lib/main_scaffold.dart
+++ lib/main_scaffold.dart
@@ -1,3 +1,4 @@
+import 'package:babel_novel/providers/keyboard_visibility_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -21,6 +22,7 @@ class MainScaffold extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final pageIndex = ref.watch(bottomNavProvider);
+    final keyboardIsVisible = ref.watch(keyboardVisibilityProvider);
 
     return Scaffold(
       body: Stack(
@@ -28,56 +30,60 @@ class MainScaffold extends ConsumerWidget {
           Positioned.fill(
             child: IndexedStack(index: pageIndex, children: _routes),
           ),
-          Positioned(
-            height: 60,
-            bottom: 10,
-            left: 20,
-            right: 20,
-            child: Material(
-              elevation: 10, // Adjust the elevation as needed
-              borderRadius: BorderRadius.circular(100),
-              clipBehavior: Clip.hardEdge,
-              child: Container(
-                decoration: const BoxDecoration(
-                  // border: Border.all(color: Colors.black12, width: 1),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(100),
+          if (keyboardIsVisible)
+            const Positioned(bottom: 0, left: 0, child: SizedBox.shrink())
+          else
+            Positioned(
+              height: 60,
+              bottom: 10,
+              left: 20,
+              right: 20,
+              child: Material(
+                elevation: 10, // Adjust the elevation as needed
+                borderRadius: BorderRadius.circular(100),
+                clipBehavior: Clip.hardEdge,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    // border: Border.all(color: Colors.black12, width: 1),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(100),
+                    ),
+                  ),
+                  // clipBehavior: Clip.hardEdge,
+                  child: BottomNavigationBar(
+                    elevation: 0, // Set elevation to 0 to avoid double shadow
+                    type: BottomNavigationBarType.fixed,
+                    unselectedItemColor: Colors.black26,
+                    selectedItemColor: Theme.of(context).colorScheme.secondary,
+                    unselectedLabelStyle:
+                        const TextStyle(color: Colors.black38),
+                    showUnselectedLabels: true,
+                    currentIndex: pageIndex,
+                    onTap: (index) {
+                      ref.read(bottomNavProvider.notifier).setPageIndex(index);
+                    },
+                    items: const [
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.home_outlined),
+                        label: 'Home',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.book),
+                        label: 'Genres',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.shelves),
+                        label: 'Shelf',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.more_horiz),
+                        label: 'Menu',
+                      ),
+                    ],
                   ),
                 ),
-                // clipBehavior: Clip.hardEdge,
-                child: BottomNavigationBar(
-                  elevation: 0, // Set elevation to 0 to avoid double shadow
-                  type: BottomNavigationBarType.fixed,
-                  unselectedItemColor: Colors.black26,
-                  selectedItemColor: Theme.of(context).colorScheme.secondary,
-                  unselectedLabelStyle: const TextStyle(color: Colors.black38),
-                  showUnselectedLabels: true,
-                  currentIndex: pageIndex,
-                  onTap: (index) {
-                    ref.read(bottomNavProvider.notifier).setPageIndex(index);
-                  },
-                  items: const [
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.home_outlined),
-                      label: 'Home',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.book),
-                      label: 'Genres',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.shelves),
-                      label: 'Shelf',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.more_horiz),
-                      label: 'Menu',
-                    ),
-                  ],
-                ),
               ),
-            ),
-          )
+            )
         ],
       ),
     );
